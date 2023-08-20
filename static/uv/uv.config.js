8906e8954b4af38449048a66ec1390e42b00bc6e
Darian <116446349+justDarian@users.noreply.github.com>
2023-08-19 20:35:15 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: "/~/devhub/",
+    prefix: "/~/load/",
     bare: "/bare/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
