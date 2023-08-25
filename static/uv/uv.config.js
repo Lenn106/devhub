2ec2cc07fadeffba1de7aa9596d86a32a9a83627
Darian <116446349+justDarian@users.noreply.github.com>
2023-08-25 19:31:27 -0400

    revert (still testing)

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: "/uv/~/",
-    bare: "https://tomp.app/",
+    bare: "/bare/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "/uv/uv.handler.js",
